60dbbce10a43f87055d404ae7cbfb463858afe61
mateoshz <mateoezeshz@gmail.com>
2026-09-14 09:19:29 -0300

    Add files via upload

---
+++ script.js/script.js.js
@@ -0,0 +1,16 @@
+﻿document.getElementById("formularioContacto")
+.addEventListener("submit", function(e){
+
+    let nombre = document.getElementById("nombre").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let asunto = document.getElementById("asunto").value.trim();
+    let mensaje = document.getElementById("mensaje").value.trim();
+
+    if(nombre === "" || email === "" || asunto === "" || mensaje === ""){
+        alert("Por favor complete todos los campos obligatorios.");
+        e.preventDefault();
+        return;
+    }
+
+    alert("Formulario enviado correctamente.");
+});
